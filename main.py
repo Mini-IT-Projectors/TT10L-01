4817b99b7947b0ff0ed29cdf2311c1e2fe8772fe
Akuma2302 <asyraaf2302@email.com>
2024-05-15 15:21:13 +0800

    Connect With html

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ app = Flask(__name__)
 # Create a route decorator
 @app.route('/')
 
+#def index():
+#    return "<h1>Hello World!</h1>"
+
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
+
+# localhost:5000/user/john
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html")
