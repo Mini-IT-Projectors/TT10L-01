097cbb0def81f9db9b46a139c77e013a4ed8ae79
Akuma2302 <asyraaf2302@email.com>
2024-05-18 00:35:51 +0800

    Custom error page

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ app = Flask(__name__)
 #def index():
 #    return "<h1>Hello World!</h1>"
 
+#FILTERS
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
+
+
 def index():
-    return render_template("index.html")
+    first_name = "Akmal"
+    stuff = "This is <strong>Bold</strong> Text"
+    
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushroom", 41]
+    return render_template("index.html",
+                           first_name = first_name,
+                           stuff=stuff,
+                           favorite_pizza=favorite_pizza)
 
 # localhost:5000/user/john
 @app.route('/user/<name>')
 
 def user(name):
-    return render_template("user.html")
+    return render_template("user.html", user_name=name)
