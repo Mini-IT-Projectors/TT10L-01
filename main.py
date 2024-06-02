d7802a95b4d2dbc4311c9b3c2b2fa248fdeec4a1
Akuma2302 <asyraaf2302@email.com>
2024-06-02 16:09:56 +0800

    Make your username display in the homepage

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 #Test Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template,session
+import csv
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask Instance
 app = Flask(__name__, static_folder='static')
+app.secret_key = 'cbjgdxgyjnges'
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+    
+def check_user(username, password):
+    if not os.path.exists('users.csv'):
+        return False
+    with open('users.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['username'] == username and row['password'] == password:
+                return True
+    return False
 # Create a route decorator
 @app.route('/')
+def home():
+    return render_template('home.html', title='Peer Review System')
 
-#def index():
-#    return "<h1>Hello World!</h1>"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        if check_user(username, password):
+            flash('Login successful!', 'success')
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
-#FILTERS
-#safe
-#capitalize
-#lower
-#upper
-#title
-#trim
-#striptags
-
-
-def index():
+@app.route('/home')
+def index(): 
     first_name = "Akmal"
     stuff = "This is <strong>Bold</strong> Text"
     
     favorite_pizza = ["Pepperoni", "Cheese", "Mushroom", 41]
+    username = session.get('username')
     return render_template("index.html",
                            first_name = first_name,
                            stuff=stuff,
-                           favorite_pizza=favorite_pizza)
+                           favorite_pizza=favorite_pizza,
+                           username=username)
 
 # localhost:5000/user/john
 @app.route('/user/<name>')
