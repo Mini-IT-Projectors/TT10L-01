5e4bbc809f0c5f212818a84d41ff2ea1b1537043
Akuma2302 <asyraaf2302@email.com>
2024-06-03 14:37:49 +0800

    Create Group Interface

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ def check_user(username, password):
                 return True
     return False
 
+def check_username(username):
+    if not os.path.exists('users.csv'):
+        return False
+    with open('users.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['username'] == username:
+                return True
+    return False
+
 #Read Admin Name
 def check_admin(username, password):
     if not os.path.exists('admin.csv'):
@@ -37,6 +47,17 @@ def check_admin(username, password):
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             if row['username'] == username and row['password'] == password:
+                return True
+    return False
+
+#Read Lecturer Name
+def check_lecturer(username) :
+    if not os.path.exists('lecturer.csv'):
+        return False
+    with open('lecturer.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['lecturer_username'] == username:
                 return True
     return False
 
@@ -86,17 +107,44 @@ def login_admin():
     return render_template('login_admin.html', title='Login', form=form)
 
 @app.route('/admin/home')
-def index_admin(): 
-    first_name = "Akmal"
-    stuff = "This is <strong>Bold</strong> Text"
+def index_admin():
     
-    favorite_pizza = ["Pepperoni", "Cheese", "Mushroom", 41]
+    groups = ["Group 1", "Group 2", "Group 3", "Group 4"]
+    no_groups = len(groups)
     username = session.get('username')
     return render_template("index_admin.html",
-                           first_name = first_name,
-                           stuff=stuff,
-                           favorite_pizza=favorite_pizza,
+                           groups = groups,
+                           no_groups = no_groups,
                            username=username)
+
+#Admin create group
+groups = []
+@app.route("/admin/create_group", methods=['GET', 'POST'])
+def create_group():
+    if request.method == "POST":
+        group_name = request.form.get("group_name")
+        group_leader = request.form.get("group_leader")
+        subject = request.form.get("subject")
+        lecturer = request.form.get("lecturer")
+        if check_username(username) and check_lecturer(lecturer):
+            groups.append({"group_name": group_name, "group_leader": group_leader, "subject": subject, "lecturer": lecturer})
+    username = session.get('username')
+    return render_template("admin_create.html",
+                           csv_data = groups,
+                           username=username)
+#Number of members
+@app.route('/admin/member_group', methods=['POST'])
+def member_group():
+    num_members = int(request.form.get('members'))
+    member_names = []
+    for i in range(num_members):
+        member_name = request.form.get(f'member_{i+1}')
+        if check_username(member_name):
+            member_names.append(member_name)
+    merged_member_names = ', '.join(member_names)
+    return render_template('member_group.html',
+                           merged_member_names=merged_member_names)
+    
 
 # localhost:5000/user/john
 @app.route('/user/<name>')
