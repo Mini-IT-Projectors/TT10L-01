ea1a2485488831d17402c309148051e33eefdcda
Akuma2302 <asyraaf2302@email.com>
2024-06-16 18:34:14 +0800

    Making update group but still in progress, done kaming delete group and fix previous issue when everytime refersh the data display will duplicate

--- main.py
+++ main.py
@@ -7,11 +7,48 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, FieldList, SelectField
 from wtforms.validators import DataRequired
-
+import pandas as pd
 
 # Create a Flask Instance
 app = Flask(__name__, static_folder='static')
 app.secret_key = 'cbjgdxgyjnges'
+
+class GroupForm(FlaskForm):
+    group_name = StringField('Group Name', validators=[DataRequired()])
+    group_leader = StringField('Group Leader',validators=[DataRequired()])
+    subject = SelectField('Subject', choices=[
+        ('Select Subject', 'Select Subject'),
+        ('Mini IT Project', 'Mini IT Project'),
+        ('Introduction To Physics', 'Introduction To Physics'),
+        ('Mathematics III', 'Mathematics III'),
+        ('Academic English', 'Academic English'),
+        ('Critical Thinking', 'Critical Thinking'),
+        ('Introduction To Digital System', 'Introduction To Digital System')
+    ], validators=[DataRequired()])
+    lecturer = SelectField('Lecturer', choices=[
+        ('Select Lecturer', 'Select Lecturer'),
+        ('Suhaini Binti Nordin', 'Suhaini Binti Nordin'),
+        ('Zubair Hassan Tarif', 'Zubair Hassan Tarif'),
+        ('Munirah Binti Munawar Ali', 'Munirah Binti Munawar Ali'),
+        ('Khairi Shazwan Bin Dollmat', 'Khairi Shazwan Bin Dollmat'),
+        ('Muhammad Effendi Bin Ismail', 'Muhammad Effendi Bin Ismail'),
+        ('Mohammad Shadab Khan', 'Mohammad Shadab Khan')
+    ], validators=[DataRequired()])
+    number_of_members = IntegerField('Number of Members', validators=[DataRequired()])
+    member_names = FieldList(StringField(f'Member Name', validators=[DataRequired()], min_entries=1))
+    submit = SubmitField('Create Group')
+
+class Group:
+    def __init__(self, group_name, group_leader, subject, lecturer, number_of_members, member_names):
+        self.group_name = group_name
+        self.group_leader = group_leader
+        self.subject = subject
+        self.lecturer = lecturer
+        self.number_of_members = number_of_members
+        self.member_names = member_names
+
+    def __repr__(self):
+        return f'Group({self.group_name}, {self.group_leader}, {self.subject}, {self.lecturer}, {self.number_of_members},{self.member_names})'
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -88,13 +125,22 @@ def login_user():
             flash('Invalid username or password', 'danger')
     return render_template('login_user.html', title='Login', form=form)
 
-groups = []
+def save():
+    global groups
+    groups = []
+    with open('groups.csv', 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # skip header row
+            for row in reader:
+                group = (Group(row[0], row[1], row[2], row[3], int(row[4]), row[5: ]))
+                groups.append(group)
+
+        
 @app.route('/user/home')
 def index_user(): 
+    global groups
     username = session.get('username')
-    groups = None
     with open('groups.csv', mode='r') as file:
-        groups = get_all_groups_from_csv()
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             if row["Group Leader"] == username or username in row['Member Names']:
@@ -169,28 +215,17 @@ def login_admin():
 
 @app.route('/admin/home')
 def index_admin():
-    
-    groups = get_all_groups_from_csv()
-    mini_it_groups = 0
-    physics_groups = 0
-    english_groups = 0
-    with open('groups.csv', mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            if row['Subject'] == "Mini IT":
-                mini_it_groups += 1
-            if row['Subject'] == "Introduction To Physics":
-                physics_groups += 1
-            if row['Subject'] == "Academic English":
-                english_groups += 1
+    global groups
+    save()
+    groups_count = {"Mini IT Project": 0, "Academic English": 0, "Introduction To Physics": 0}
+    for group in groups:
+        groups_count[group.subject] += 1
         
     
     username = session.get('username')
     return render_template("index_admin.html",
                             groups = groups,
-                            mini_it_groups = mini_it_groups,
-                            physics_groups = physics_groups,
-                            english_groups = english_groups,
+                            groups_count = groups_count,
                             username = username)
 #Admin create group
 
@@ -204,53 +239,28 @@ def add_group_to_csv(group, member_names):
         writer = csv.writer(csvfile)
         writer.writerow([group.group_name, group.group_leader, group.subject, group.lecturer, group.number_of_members, ', '.join(member_names)])
 
-def get_all_groups_from_csv():
-    with open('groups.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  # skip header row
-        for row in reader:
-            group = (Group(row[0], row[1], row[2], row[3], row[4], row[5: ]))
-            groups.append(group)
-    return groups
+def write_groups_to_csv(groups):
+    fieldnames = ['Group Name', 'Group Leader', 'Subject', 'Lecturer', 'Number of Members', 'Member Names']
+    with open('groups.csv', 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for group in groups:
+            writer.writerow({
+                'Group Name': group.group_name,
+                'Group Leader': group.group_leader,
+                'Subject': group.subject,
+                'Lecturer': group.lecturer,
+                'Number of Members': group.number_of_members,
+                'Member Names': ','.join(group.member_names)
+            })
 
-class GroupForm(FlaskForm):
-    group_name = StringField('Group Name', validators=[DataRequired()])
-    group_leader = StringField('Group Leader',validators=[DataRequired()])
-    subject = SelectField('Subject', choices=[
-        ('Select Subject', 'Select Subject'),
-        ('Mini IT Project', 'Mini IT Project'),
-        ('Introduction To Physics', 'Introduction To Physics'),
-        ('Mathematics III', 'Mathematics III'),
-        ('Academic English', 'Academic English'),
-        ('Critical Thinking', 'Critical Thinking'),
-        ('Introduction To Digital System', 'Introduction To Digital System')
-    ], validators=[DataRequired()])
-    lecturer = SelectField('Lecturer', choices=[
-        ('Select Lecturer', 'Select Lecturer'),
-        ('Suhaini Binti Nordin', 'Suhaini Binti Nordin'),
-        ('Zubair Hassan Tarif', 'Zubair Hassan Tarif'),
-        ('Munirah Binti Munawar Ali', 'Munirah Binti Munawar Ali'),
-        ('Khairi Shazwan Bin Dollmat', 'Khairi Shazwan Bin Dollmat'),
-        ('Muhammad Effendi Bin Ismail', 'Muhammad Effendi Bin Ismail'),
-        ('Mohammad Shadab Khan', 'Mohammad Shadab Khan')
-    ], validators=[DataRequired()])
-    number_of_members = IntegerField('Number of Members', validators=[DataRequired()])
-    member_names = FieldList(StringField(f'Member Name', validators=[DataRequired()], min_entries=1))
-    submit = SubmitField('Create Group')
-
-
-class Group:
-    def __init__(self, group_name, group_leader, subject, lecturer, number_of_members, member_names):
-        self.group_name = group_name
-        self.group_leader = group_leader
-        self.subject = subject
-        self.lecturer = lecturer
-        self.number_of_members = number_of_members
-        self.member_names = member_names
-
-    def __repr__(self):
-        return f'Group({self.group_name}, {self.group_leader}, {self.subject}, {self.lecturer}, {self.number_of_members},{self.member_names})'
-    
+def find_group_by_name(group_name,subject):
+    global groups
+    for group in groups:
+        if group.group_name == group_name and group.subject == subject:
+            return group
+    return None
+ 
 @app.route("/admin/create_group", methods=['GET','POST'])
 def create_group():
     form = GroupForm()
@@ -273,58 +283,59 @@ def create_group():
 @app.route("/admin/groups_list")
 def groups_list():
     # Assuming you have a list of Group objects called groups
-    groups = get_all_groups_from_csv()
+    global groups
 
     return render_template('admin_groups_list.html', groups=groups)    
 
-@app.route('/dashboard', methods=['GET', 'POST'])
-def dashboard():
-    form = UserForm()
-    csv_file = 'groups.csv'
-
+@app.route('/admin/update_group_<string:group_name>', methods=['GET', 'POST'])
+def update_group(group_name,subject):
+    global groups
+    form = GroupForm()
+    group_to_update = find_group_by_name(group_name,subject)
     if request.method == "POST":
-        name_to_update = {}
-        with open(csv_file, 'r', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['id'] == current_user.id:
-                    name_to_update = row
-                    break
+        group_to_update.group_name = form.group_name.data
+        group_to_update.group_leader = form.group_leader.data
+        group_to_update.subject = form.subject.data
+        group_to_update.lecturer = form.lecturer.data
+        group_to_update.number_of_members = form.number_of_members.data
+        group_to_update.member_names = [field.data for field in form.member_names.entries if field.data]
+        
+        for i, group in enumerate(groups):
+            if group.group_name == group_name:
+                groups[i] = group_to_update
+                break
 
-        if name_to_update:
-            name_to_update['name'] = request.form['name']
-            name_to_update['email'] = request.form['email']
-            name_to_update['favorite_color'] = request.form['favorite_color']
-            name_to_update['username'] = request.form['username']
-            name_to_update['about_author'] = request.form['about_author']
+        write_groups_to_csv(groups)
+        flash("Group updated successfully", "success")
+        return redirect(url_for('groups_list'))
+    if request.method == 'GET':
+        form.group_name.data = group_to_update.group_name
+        form.group_leader.data = group_to_update.group_leader
+        form.subject.data = group_to_update.subject
+        form.lecturer.data = group_to_update.lecturer
+        form.number_of_members.data = group_to_update.number_of_members
+        form.member_names.entries = [StringField('Member Name', default=name) for name in group_to_update.member_names]
 
-            # Check for profile pic
-            if request.files['profile_pic']:
-                pic_filename = secure_filename(request.files['profile_pic'].filename)
-                pic_name = str(uuid.uuid1()) + "_" + pic_filename
-                request.files['profile_pic'].save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))
-                name_to_update['profile_pic'] = pic_name
+    return render_template("update.html", form=form, group_to_update=group_to_update)
 
-            with open(csv_file, 'w', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=name_to_update.keys())
-                writer.writeheader()
-                writer.writerow(name_to_update)
+@app.route("/admin/delete_group_<string:group_name>_<string:subject>", methods=['GET', 'POST'])
+def delete_group(group_name,subject):
+    try:
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv('groups.csv')
 
-            flash("User Updated Successfully!")
-            return render_template("dashboard.html", form=form, name_to_update=name_to_update)
-        else:
-            flash("Error!  Looks like there was a problem...try again!")
-            return render_template("dashboard.html", form=form, name_to_update={})
-    else:
-        with open(csv_file, 'r', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['id'] == current_user.id:
-                    return render_template("dashboard.html", form=form, name_to_update=row)
+        # Filter the DataFrame to exclude the row with the specified group_name and subject
+        df_filtered = df[~((df['Group Name'] == group_name) & (df['Subject'] == subject))]
 
-    return render_template('dashboard.html')
+        # Write the filtered DataFrame back to the CSV file
+        df_filtered.to_csv('groups.csv', index=False)
 
-
+        flash("Group deleted successfully")
+    except Exception as e:
+        flash(f"Whoops! There was a problem deleting the group: {str(e)}")
+    
+    return redirect(url_for('index_admin'))     
+    
 @app.route("/admin/dashboard")
 def admin_dashboard():
     csv_file = "groups.csv"
